d8ad1f2e61e835dbeedca3866519a5e929563fb6
tejas-pagare <147067782+tejas-pagare@users.noreply.github.com>
2025-03-14 12:57:13 +0530

    added updated url to endpoint

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose"
 const dbConnection = async()=>{
 try {
   await mongoose.connect(process.env.MONGO_URL);
+  console.log("Database connection established  ")
   } catch (error) {
   console.log("Error connecting to MongoDB");
   process.exit(1);
--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,4 @@
-export const USER_API_ENDPOINT="http://localhost:8000/api/v1/user"
-export const Job_API_ENDPOINT="http://localhost:8000/api/v1/job"
-export const Application_API_ENDPOINT="http://localhost:8000/api/v1/application"
-export const Company_API_ENDPOINT="http://localhost:8000/api/v1/company"
+export const USER_API_ENDPOINT="https://myjobportal-iwc6.onrender.com/api/v1/user"
+export const Job_API_ENDPOINT="https://myjobportal-iwc6.onrender.com/api/v1/job"
+export const Application_API_ENDPOINT="https://myjobportal-iwc6.onrender.com/api/v1/application"
+export const Company_API_ENDPOINT="https://myjobportal-iwc6.onrender.com/api/v1/company"
